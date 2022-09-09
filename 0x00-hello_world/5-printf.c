4886becb661fe9248aeb5416e77c4f05a0ddc246
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-08 22:31:46 -0700

    Task 5-7

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,17 +2,16 @@
 
 
 /**
- * main - Print the string in the put function
+ * main - print the string in the put function
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Successful)
  */
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of      	art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 
 }
