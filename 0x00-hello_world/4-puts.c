4d417c91648db669028e04bca8e4d932501bac04
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-08 22:03:38 -0700

    Task 4-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - print the string in the put function
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
