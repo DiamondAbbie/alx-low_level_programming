2dc7dec282d899612e3fc9573252a79fdc12ac7d
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-15 20:13:13 -0700

    Advanced C function and nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,7 @@
 void print_triangle(int size)
 {
 int sp, ro, tr;
+
 if (size <= 0)
 {
 _putchar('\n');
@@ -16,7 +17,8 @@ _putchar('\n');
 else
 {
 for (ro = 0; ro <= (size - 1); ro++)
-{for (sp = 0; sp < (size - 1) - ro; sp++)
+{
+for (sp = 0; sp < (size - 1) - ro; sp++)
 {
 _putchar(' ');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_most_numbers - a function that prints the numbers, from 0 to 9
- * Return 0-9, excluding 2 and 4, followed by a new line
+ * print_most_numbers - a function that prints the numbers except 2 and 4
+ * Return: 0
  */
 
 void print_most_numbers(void)
 {
-int i;
-for(i = 0; i <= 9; i++)
+int n;
+
+for (n = 48; n < 58; n++)
 {
-if ((i == 2) || (i == 4))
+if ((n == 50) || (n == 52))
+{
 continue;
-else
-_putchar(i + '0');
 }
-_putchar('\n');
+_putchar(n);
+}
+_putchar(10);
 }
