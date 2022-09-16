f49b8d63788df0d95f608af35c13283ae40b5346
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-15 21:08:15 -0700

    Advanced C function and nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_triangle - a function that prints a triangle
+ * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
- * Return: 0
  */
 
 void print_triangle(int size)
 {
-int inc1, inc2;
-
-if (size > 0)
+if (size <= 0)
 {
-for (inc1 = 1; inc1 <= size; inc1++)
-{
-for ((inc2 = size - inc1); inc2 > 0; inc2--)
-{
-_putchar(' ');
+_putchar('\n');
 }
-for (inc2 = 0; inc2 < inc1; inc2++)
+else
+{
+int i, j;
+
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size, j++)
+{
+_putchar('');
+}
+
+for (j = 1; j<=i; j++)
 {
 _putchar('#');
 }
-if (inc1 == size)
-{
-continue;
-}
- _putchar('\n');
+
+_putchar('\n');
 }
 }
 }
