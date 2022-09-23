77a214e603665cdec7f95eb296448a210808d348
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-22 20:43:07 -0700

    Function to print array of integers in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * Reverse_array - Reverses the content of an array of integers
- * @a: Array identifier
- * @n: Max length of array
- * Return: Void
+ * reverse_array - Reverses the content of an array of integers
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
  */
 
 void reverse_array(int *a, int n)
 {
-int i = 0, j;
-int temp;
+int tmp, index;
 
-for (i = 0; i < n - 1; i++)
+for (index = n - 1; index >= n / 2; index--)
 {
-for (j = i + 1; j > 0; j--)
-{
-temp = *(a + j);
-*(a + j) = *(a + (j - 1));
-*(a + (j - 1)) = temp;
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
 }
 }
-
-}
-
