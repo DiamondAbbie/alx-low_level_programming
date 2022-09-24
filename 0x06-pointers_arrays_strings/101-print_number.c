0b93994fed5695864af202cfa42a93d37d583691
DiamondAbbie <ajibiseabigail@gmail.com>
2022-09-23 19:48:36 -0700

    function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
-*print_number - prints an integer.
-*
-*@n: integer to be printed
-*
-*Return: void
+* print_number - prints an integer.
+* @n: integer to be printed
 */
-
 void print_number(int n)
 {
 	unsigned int num = n;
